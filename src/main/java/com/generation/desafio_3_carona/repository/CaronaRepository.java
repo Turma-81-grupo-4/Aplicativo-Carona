4f1fe8a729c36bc1189ac249b92fb50238bd27af
Henrique Machado <henriquesousa@ymail.com>
2025-05-06 12:14:44 -0300

    Criação de controller e repo para 'caronas'

--- src/main/java/com/generation/desafio_3_carona/repository/CaronaRepository.java
+++ src/main/java/com/generation/desafio_3_carona/repository/CaronaRepository.java
@@ -1,5 +1,12 @@
 package com.generation.desafio_3_carona.repository;
 
-public interface CaronaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.desafio_3_carona.model.Carona;
+
+public interface CaronaRepository extends JpaRepository<Carona, Long>{
+	public List<Carona> findAllByDestinoContainingIgnoreCase(@Param("destino") String destino);
 }
