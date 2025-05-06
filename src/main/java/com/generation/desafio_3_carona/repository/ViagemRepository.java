7afc3a4ba19593ea727841cd90cde346bd49a864
Henrykeeh <c.henrique.silvabarbosa@gmail.com>
2025-05-06 12:20:47 -0300

    Criação do CRUD da entidade Viagem

---
+++ src/main/java/com/generation/desafio_3_carona/repository/ViagemRepository.java
@@ -0,0 +1,12 @@
+package com.generation.desafio_3_carona.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.desafio_3_carona.model.Viagem;
+
+public interface ViagemRepository extends JpaRepository<Viagem, Long> {
+	public List<Viagem> findAllByTipoViagemContainingIgnoreCase(@Param("tipoViagem") String tipoViagem);
+}
