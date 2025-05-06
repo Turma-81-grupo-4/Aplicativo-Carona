6780723111f6ff270e01556b70d45370895ff8be
CahTarine <camille.tarine@gmail.com>
2025-05-06 12:23:59 -0300

    cria controller e repository usuario

---
+++ src/main/java/com/generation/desafio_3_carona/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.generation.desafio_3_carona.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.desafio_3_carona.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	public Optional<Usuario> findByUsuario(String Usuario);
+}
