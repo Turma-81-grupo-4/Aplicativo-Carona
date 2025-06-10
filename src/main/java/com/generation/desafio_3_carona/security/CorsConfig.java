a0aeef4b34e251a0a6a2a6ebf2d0750c43beac37
Henrykeeh <c.henrique.silvabarbosa@gmail.com>
2025-06-10 02:32:37 -0300

    ajuste/liberações no CORS-nova classe global

---
+++ src/main/java/com/generation/desafio_3_carona/security/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.generation.desafio_3_carona.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:5173",
+                "https://carona-nu.vercel.app",
+                "https://carona-grupo-4-java-81s-projects.vercel.app"
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
